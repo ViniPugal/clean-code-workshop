904363c33b2933b506e75a0fe9c1f6bf8ef0d88a
Vinitha Pukazh Bagya R <vinitha.pukazh@thoughtworks.com>
2021-12-03 17:38:42 +0530

    [Hareini/Vinitha] Refactor total amount and frequent rental points

--- src/main/java/com/thoughtworks/movierental/Rental.java
+++ src/main/java/com/thoughtworks/movierental/Rental.java
@@ -16,4 +16,41 @@ public class Rental {
     public Movie getMovie() {
         return movie;
     }
+
+    public double amount() {
+        double amount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (daysRented > 2)
+                    amount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount += 1.5;
+                if (daysRented > 3)
+                    amount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
+
+    public int frequentRenterPoints() {
+        int frequentRenterPoints = 1;
+        if (isBonusApplicable()) frequentRenterPoints++;
+        return frequentRenterPoints;
+    }
+
+    private boolean isBonusApplicable() {
+        return movie.isNewRelease()
+                &&
+                isMoreThanOneRentalDay();
+    }
+
+    private boolean isMoreThanOneRentalDay() {
+        return daysRented > 1;
+    }
+
 }
